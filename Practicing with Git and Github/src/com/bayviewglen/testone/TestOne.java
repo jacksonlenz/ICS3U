d4531cdeab185c7d1de146a8f9d6ecd06c27d6e9
jacksonlenz <jlenz@bayviewglen.ca>
2014-10-31 20:08:24 -0400

    Push One

---
+++ Practicing with Git and Github/src/com/bayviewglen/testone/TestOne.java
@@ -0,0 +1,11 @@
+package com.bayviewglen.testone;
+
+public class TestOne {
+
+	public static void main(String[] args) {
+		double x = 5.44;
+		System.out.println(x);
+
+	}
+
+}
---
+++ Tets/src/com/practice/lol/TestIng.java
@@ -0,0 +1,12 @@
+package com.practice.lol;
+
+public class TestIng {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("lol");
+		
+	}
+
+}
